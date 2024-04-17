7ab1cb0f7c05e90d8e1cfcccdfd081a453e24a3d
keee <1002037953@qq.com>
2024-04-17 10:57:27 +0800

    推理脚本的输入修改与进度条增加

--- test.py
+++ test.py
@@ -10,25 +10,31 @@ from torch.autograd import Variable
 from torchvision import transforms
 from model.utils.cli_helper_test import parse_args
 import numpy as np
+import os
+from tqdm import tqdm
+import time
 from PIL import Image
 import pandas as pd
 import cv2
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 def load_test_data(img_path, transform):
     img = Image.open(img_path)
     img = transform(img)
     return img
 
-# TODO:将推理脚本改为输入读取文件夹，读取文件夹里的所有的图片进行推理，这样的参数需要放在args里（相当于给后端预留调用接口）
-# TODO:读取文件夹图片推理的过程也加上进度条
 
 def predict():
-    if os.path.exists('test_output') == False:
-        os.mkdir('test_output')
     args = parse_args()
-    img_path = args.img
+    img_fold = args.img_fold
+    # 图片文件路径列表
+    files = os.listdir(img_fold)
+    # 生成预测图片的文件夹保存路径
+    output_dir = os.path.join(img_fold, 'test_output')
+    if os.path.exists(output_dir) == False:
+        os.mkdir(output_dir)
     resize_height = args.height
     resize_width = args.width
 
@@ -45,20 +51,30 @@ def predict():
     model.eval()
     model.to(DEVICE)
 
-    dummy_input = load_test_data(img_path, data_transform).to(DEVICE)
-    dummy_input = torch.unsqueeze(dummy_input, dim=0)
-    outputs = model(dummy_input)
+    # 使用 tqdm 包裹 files，显示进度条
+    with tqdm(total=len(files), desc="Processing files", unit="file", bar_format="{l_bar}{bar:40}{r_bar}") as pbar:
+        for path in files:
+            img_path = os.path.join(img_fold, path)
 
-    input = Image.open(img_path)
-    input = input.resize((resize_width, resize_height))
-    input = np.array(input)
+            dummy_input = load_test_data(img_path, data_transform).to(DEVICE)
+            dummy_input = torch.unsqueeze(dummy_input, dim=0)
+            outputs = model(dummy_input)
 
-    instance_pred = torch.squeeze(outputs['instance_seg_logits'].detach().to('cpu')).numpy() * 255
-    binary_pred = torch.squeeze(outputs['binary_seg_pred']).to('cpu').numpy() * 255
+            input = Image.open(img_path)
+            input = input.resize((resize_width, resize_height))
+            input = np.array(input)
 
-    cv2.imwrite(os.path.join('test_output', 'input.jpg'), input)
-    cv2.imwrite(os.path.join('test_output', 'instance_output.jpg'), instance_pred.transpose((1, 2, 0)))
-    cv2.imwrite(os.path.join('test_output', 'binary_output.jpg'), binary_pred)
+            instance_pred = torch.squeeze(outputs['instance_seg_logits'].detach().to('cpu')).numpy() * 255
+            binary_pred = torch.squeeze(outputs['binary_seg_pred']).to('cpu').numpy() * 255
+
+            cv2.imwrite(os.path.join(output_dir, path.split('.')[0] + '_input.jpg'), input)
+            cv2.imwrite(os.path.join(output_dir, path.split('.')[0] + '_instance_output.jpg'), instance_pred.transpose((1, 2, 0)))
+            cv2.imwrite(os.path.join(output_dir, path.split('.')[0] + '_binary_output.jpg'), binary_pred)
+
+            # 更新进度条
+            pbar.update(1)
+
+    print('output is saved:', output_dir)
 
 
 if __name__ == "__main__":
